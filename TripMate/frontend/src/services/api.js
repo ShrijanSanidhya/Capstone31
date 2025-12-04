0a41ef31c8edfdddb367e5c0dc5f5a8848416579
Shrijan Sanidhya <your_email@example.com>
2025-12-04 23:08:25 +0530

    updated base url

--- TripMate/frontend/src/services/api.js
+++ TripMate/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://tripplanner-8pge.onrender.com/api',
     withCredentials: true,
 });
 
