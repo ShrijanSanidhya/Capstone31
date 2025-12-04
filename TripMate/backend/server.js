2929b9a108ceb29084136d0bb41aa90452c4bd70
Shrijan Sanidhya <your_email@example.com>
2025-12-04 23:12:54 +0530

    updated cors production url

--- TripMate/backend/server.js
+++ TripMate/backend/server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5001;
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173", 
+        "https://capstone31.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
